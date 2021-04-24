2fdc68712959828466174a54f286a469738afd17
Kacper Krzywiec <krzywiec@gmail.com>
2021-04-24 07:47:02 +0200

    wip: add upload button

--- app/src/client/App/index.tsx
+++ app/src/client/App/index.tsx
@@ -1,12 +1,46 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Icon,
+  ChakraProvider,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { FiFile } from "react-icons/fi";
 
 import type { GpxModel } from "../../lambda/models/gpx";
 
 import { Map } from "../Map";
+import { FileUploader } from "../FileUploader";
 import { GET_GPXS } from "./queries";
 
+type FormValues = {
+  file_: FileList;
+};
 export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const onSubmit = handleSubmit((data) => console.log("On Submit: ", data));
+
+  const validateFiles = (value: FileList) => {
+    if (value.length < 1) {
+      return "Files is required";
+    }
+    for (const file of Array.from(value)) {
+      const fsMb = file.size / (1024 * 1024);
+      const MAX_FILE_SIZE = 10;
+      if (fsMb > MAX_FILE_SIZE) {
+        return "Max file size 10mb";
+      }
+    }
+    return true;
+  };
   const { data, loading, error } = useQuery<{ gpxs: GpxModel[] }>(GET_GPXS);
   if (loading) {
     return <div>Loading...</div>;
@@ -21,8 +55,9 @@ export default function App() {
   }
 
   return (
-    <div style={{ textAlign: "center" }}>
-      {/* <ul>
+    <ChakraProvider>
+      <div style={{ textAlign: "center" }}>
+        {/* <ul>
         {data?.gpxs.map(({ content, title }, index: number) => (
           <li key={index}>
             <h3>{title}</h3>
@@ -30,7 +65,27 @@ export default function App() {
           </li>
         ))}
       </ul> */}
-      {data && <Map gpx={data.gpxs[0].content} />}
-    </div>
+        {/* {data && <Map gpx={data.gpxs[0].content} />} */}
+        <form onSubmit={onSubmit}>
+          <FormControl isInvalid={!!errors.file_} isRequired>
+            <FormLabel>{"File input"}</FormLabel>
+
+            <FileUploader
+              accept={"image/*"}
+              multiple
+              register={register("file_", { validate: validateFiles })}
+            >
+              <Button leftIcon={<Icon as={FiFile} />}>Upload</Button>
+            </FileUploader>
+
+            <FormErrorMessage>
+              {errors.file_ && errors?.file_.message}
+            </FormErrorMessage>
+          </FormControl>
+
+          <button>Submit</button>
+        </form>
+      </div>
+    </ChakraProvider>
   );
 }
