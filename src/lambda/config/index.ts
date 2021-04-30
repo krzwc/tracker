bf204809ef267f599ce564b2a7e94cc17ba2b4f4
Kacper Krzywiec <krzywiec@gmail.com>
2021-04-30 12:15:37 +0200

    chore: add production graphql endpoint to rest config #2

--- src/lambda/config/index.ts
+++ src/lambda/config/index.ts
@@ -1,6 +1,6 @@
 export const isDev = process.env.NODE_ENV === "development";
 
-export const productionURL = "https://cocky-joliot-9e0c46.netlify.app:3000";
+export const productionURL = "https://cocky-joliot-9e0c46.netlify.app";
 export const URL = isDev ? "http://localhost:3000" : productionURL;
 
 export const DATABASE_URL = isDev
